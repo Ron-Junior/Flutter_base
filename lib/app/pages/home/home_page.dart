406e05ef320a0b98aefdd155a9b4a3b15a94ce07
ron_jr <ronaldoj.w2007@gmail.com>
2019-10-31 23:25:53 -0300

    implements push notification

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseMessaging _messaging = FirebaseMessaging();
+  @override
+  void initState(){
+    super.initState();
+    
+    _messaging.getToken().then((token) {
+      print(token);
+      print("xablau");
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
