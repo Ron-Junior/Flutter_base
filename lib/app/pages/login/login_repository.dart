6d4f7bf6d5104ce89afc4b4993b087de644645da
ron_jr <ronaldoj.w2007@gmail.com>
2019-11-11 09:28:22 -0300

    fix login

--- lib/app/pages/login/login_repository.dart
+++ lib/app/pages/login/login_repository.dart
@@ -12,8 +12,8 @@ class LoginRepository extends Disposable {
   Future<Object> login() async {
     data = new User();
     data.email = "ronaldo@email.com";
-    data.password = 123;
-    data = data.toJsom();
+    data.encryptedPassword = "123";
+    data = data.toJson();
     try{
       final res = await _dio.post("/v1/users/login", data: this.data);
       return res;
