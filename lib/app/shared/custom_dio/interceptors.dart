d7e8b89b4bc736221c2cffb76d7854ef7476b0ea
ron_jr <ronaldoj.w2007@gmail.com>
2019-11-12 08:43:39 -0300

    fix login

--- lib/app/shared/custom_dio/interceptors.dart
+++ lib/app/shared/custom_dio/interceptors.dart
@@ -3,11 +3,13 @@ import 'package:flutter_base/app/app_module.dart';
 import 'package:flutter_base/app/shared/auth/auth_bloc.dart';
 
 class BaseInterceptors extends InterceptorsWrapper {
-
+ var token = AppModule.to.getBloc<AuthBloc>().token;
   @override
   Future onRequest(RequestOptions options) {
-    //options.headers = {'Authorization': AppModule.to.getBloc<AuthBloc>().token};
-    // options.headers = {'Content-Type': 'multipart/form-data'};
+    // if(token !=  null){
+    //  options.headers = {'Authorization': token};
+    // }
+    // options.headers = {'Content-Type': 'application/json'};
     print('onrequest - ${options.data}');
     print(AppModule.to.getBloc<AuthBloc>().token);
     return super.onRequest(options);
